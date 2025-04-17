7328170def17f1c81834f9e9f1edd481520d9225
Narendra Singh <narendrasinghsitu4@gmail.com>
2025-04-17 16:19:22 +0530

    resume link updated

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () =>{
 
 
     const handleDownload = () => {
-        const fileId = "168CjhllRj4Tu6LfQ095agcPPt5rzl1Sh";
+        const fileId = "1BZj1s249t4Y2ACkCwsgCjMtdlIQnyAsF";
         const viewUrl = `https://drive.google.com/file/d/${fileId}/view`;
         const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
     
